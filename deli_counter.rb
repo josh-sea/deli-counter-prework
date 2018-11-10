5a97050cd8bfd09525b1ff51b33769b6dbe63ff3
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-10 19:06:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,9 +14,8 @@ def take_a_number(array,name)
   puts "Welcome, #{name}. You are number #{array.length+1} in line"
 end
 
-  take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
-  take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
-  take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+def now_serving(array)
+  puts "Currently serving "
  
   line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
  
