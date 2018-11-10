47f3d2af6020843d17f6acbc7fff812c9162a26d
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-10 19:10:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,7 @@ def line(array)
   end
 end
 
+# take a number method
 def take_a_number(array,name)
   puts "Welcome, #{name}. You are number #{array.length+1} in line"
 end
@@ -22,16 +23,18 @@ def now_serving(array)
     array.shift(array[0])
   end
 end
+
+
 #  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
- 
-  now_serving(katz_deli) #=> "Currently serving Ada."
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
- 
-  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
- 
-  now_serving(katz_deli) #=> "Currently serving Grace."
- 
-  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
+# 
+#  now_serving(katz_deli) #=> "Currently serving Ada."
+# 
+#  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+# 
+#  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in #line.
+#
+#  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+# 
+#  now_serving(katz_deli) #=> "Currently serving Grace."
+# 
+#  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
